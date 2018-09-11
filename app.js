e450c495a277e771b80fc235aed9707732906d99
Mitch Lembke <mitchell.lembke@gmail.com>
2018-09-11 09:48:43 -0400

    removed users route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.disable('x-powered-by');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
 app.use('/login', loginRouter);
 
