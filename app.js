dcd9b4f5474d977fb3e519970f014431db6e09d8
Mitch Lembke <mitchell.lembke@gmail.com>
2018-09-25 11:58:15 -0400

    Changed CORS header to allow process.env.API_URL or localhost:3001

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001')
+  res.header('Access-Control-Allow-Origin', (process.env.API_URL || 'http://localhost:3001'))
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE, PUT')
