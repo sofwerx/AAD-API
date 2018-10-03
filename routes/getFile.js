a2ba0edca009a97a19eb7bf7a89ae3c314c24918
Mitch Lembke <mitchell.lembke@gmail.com>
2018-10-03 16:51:51 -0400

    Sending back buffer array instead of base64Content

--- routes/getFile.js
+++ routes/getFile.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const getFile = (req, res, next) => {
     fs.readFile(
-        `./public/uploads/${req.params.path}`, 'base64',
-        (err, base64Content) => {
-            if(base64Content){
-                return res.send({file: base64Content, review_id: req.params.reviewId, path: `./public/uploads/${req.params.path}`} );
+        `./public/uploads/${req.params.path}`,
+        (err, bufferArr) => {
+            if(bufferArr){
+                return res.send({file: bufferArr, review_id: req.params.reviewId, path: `./public/uploads/${req.params.path}`} );
             } else {
                 return res.send({file: 'none'})
             }
