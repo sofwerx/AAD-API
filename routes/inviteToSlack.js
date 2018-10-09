4858e8578d88ecc9647686536df382e9140d92da
Ian Blenke <ian@blenke.com>
2018-10-09 14:39:09 -0400

    Adding some debugging for slack signup

--- routes/inviteToSlack.js
+++ routes/inviteToSlack.js
@@ -8,15 +8,22 @@ const inviteToSlack = (req, res, next) => {
     const token = process.env.SLACK_TOKEN; 
     
     const url = 'https://'+ slackTeam + '.slack.com/api/users.admin.invite';
+    console.log("Inviting to " + slackTeam + " url=" + url);
     return fetch(url, { 
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: "token="+ token + "&email=" + req.body.email
     })
     .then(res => {
-        return res
+        res.json().then(json => {
+          console.log("Invited! json=" + JSON.stringify(json));
+	});
+        return res;
     })
-    .catch(err => console.log('Invite to Slack Failed', err));
+    .catch(err => {
+    		console.log('Invite to Slack Failed', err)
+	}
+    );
 }
 
     
