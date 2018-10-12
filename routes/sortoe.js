d3846a1cc214cc1208058f26d42e5074e9401d94
Mitch Lembke <mitchell.lembke@gmail.com>
2018-10-12 15:05:21 -0400

    Changing header to 'no-cache'

--- routes/sortoe.js
+++ routes/sortoe.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.set('Cache-Control', 'private');
+  res.set('Cache-Control', 'no-cache');
   res.render('sortoe');
 });
 
