d298775c7ed0e369bd20f5ec7cdaa05fd975c06c
Mitch Lembke <mitchell.lembke@gmail.com>
2018-11-21 14:10:24 -0500

    removed console.logs in updateUsername

--- routes/updateUsername.js
+++ routes/updateUsername.js
@@ -3,9 +3,6 @@ const router = express.Router()
 const knex = require('../knex')
 
 const updateUsername = (req, res, next) => {
-  console.log("here is the req.body", req.body)
-  console.log("Here is the current username", req.body.currentUsername)
-  console.log("Here is the new username", req.body.newUsername)
   knex('users')
   .where("username", req.body.currentUsername)
   .update({ username: req.body.newUsername })
