f47790a94b51389e82340e350f7c586bfb9e8bff
Peter W. Purcell <peterwpurcell@gmail.com>
2019-03-15 19:32:13 -0500

    Revert "Revert "Add functionality for SurveyResponse Page.""
    This reverts commit d5011c5e491ac5d76788c40290d82a9d20c0658b.

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,11 @@
 const DATABASE_CLIENT = process.env.DATABASE_CLIENT || 'pg';
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost/aad';
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:example@localhost:32768/postgres';
 
 module.exports = {
 
   development: {
     client: 'postgres',
     connection: DATABASE_URL,
-    // LOCAL TESTING Credentials
-    // database: 'postgres',
-    // user: 'postgres',
-    // password: 'example',
-    // port: 5432,
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/db/migrations`
--- server/models/answer_model.js
+++ server/models/answer_model.js
@@ -13,8 +13,10 @@ const selectableProps = [
 ];
 
 const selectablePropsForSurveyResponse = [
-  'question_order',
   'question_id',
+  'QuestionType.short_name as question_type',
+  'question_text',
+  'question_order',
   'answer_text',
   'answer_numeric',
   'answer_bool'
@@ -32,6 +34,7 @@ module.exports = (knex) => {
     return knex.select(...selectablePropsForSurveyResponse)
       .from('Answer')
       .join('Question', 'Question.id', 'Answer.question_id')
+      .join('QuestionType', 'QuestionType.id', 'Question.question_type_id')
       .where({ survey_response_id: surveyResponseId })
       .orderBy('question_order', 'asc')
       .timeout(1000);
